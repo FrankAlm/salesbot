f8b52882ea141145fe35350d91b0e40ec09e41f9
FrankAlm <email@digitalsuccess.eu>
2025-06-03 14:24:58 +0200

    Add files via upload

---
+++ deepgram.js
@@ -0,0 +1,11 @@
+
+const axios = require('axios');
+exports.transcribe = async (audioBuffer) => {
+  const res = await axios.post('https://api.deepgram.com/v1/listen', audioBuffer, {
+    headers: {
+      'Authorization': `Token ${process.env.DEEPGRAM_API_KEY}`,
+      'Content-Type': 'audio/wav',
+    },
+  });
+  return res.data.results.channels[0].alternatives[0].transcript;
+};
---
+++ elevenlabs.js
@@ -0,0 +1,25 @@
+
+const axios = require('axios');
+const fs = require('fs');
+
+exports.speak = async (text, voiceId) => {
+  const res = await axios.post(
+    `https://api.elevenlabs.io/v1/text-to-speech/${voiceId}/stream`,
+    {
+      text: text,
+      model_id: "eleven_monolingual_v1",
+      voice_settings: { stability: 0.5, similarity_boost: 0.8 }
+    },
+    {
+      headers: {
+        "xi-api-key": process.env.ELEVEN_API_KEY,
+        "Content-Type": "application/json",
+      },
+      responseType: 'arraybuffer'
+    }
+  );
+
+  const filename = `audio-${Date.now()}.mp3`;
+  fs.writeFileSync(filename, res.data);
+  return `http://your-server.com/${filename}`;
+};
